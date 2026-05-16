e0dbd28d451a77fc98de3e875047a1c3bcc9914a
lighterthanair7 <lighterthanair7@proton.me>
2026-05-16 14:13:28 +0200

    feat[dev]: Added eko-kos and soundwave projects

--- portfolio-site/public/index.php
+++ portfolio-site/public/index.php
@@ -1,4 +1,15 @@
 <?php
+?>
 
-echo "Hello World iz FrankenPHP-a! Ovo je moj portfolio.";
-echo "Test 1";
+<h1>Moj portfolio</h1>
+<p>Evo su moji projekti:</p>
+<ul>
+    <li><a href="">- Soundwave</a></li>
+    <li><a href="">- Geo Media</a></li>
+    <li><a href="">- Exclusive villas Croatia</a></li>
+    <li><a href="">- Human digital intelligence</a></li>
+    <li><a href="">+ Eko kos</a></li>
+    <li><a href="">- Slash</a></li>
+    <li><a href="">- Fictional university</a></li>
+    <li><a href="">- Card game</a></li>
+</ul>
